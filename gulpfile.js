79f3882d2d66ca0b9cbf314b7fe6a7ff98304c96
Leandro Suza <leandro.dasilvati7@gmail.com>
2018-03-21 00:13:19 -0300

    Finalizando a instalação dos plugins e finalizando o topo do header

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var destOrigin = 'sass/**/*.sass';
+var destSaida =  'css';
 
 
 gulp.task('sass', function(){
-	return gulp.src('sass/**/*.sass')
+	return gulp.src(destOrigin)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('css'));
+		.pipe(gulp.dest(destSaida));
 	});
 
 gulp.task('watch',function(){
-	gulp.watch('sass/**/*.sass', ['sass']);
+	gulp.watch(destOrigin, ['sass']);
 
 	});
 
 gulp.task('default',['sass','watch']);
+
