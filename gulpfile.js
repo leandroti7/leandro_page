77437ce3b9cb46ed5eb1d8bde1eab728a50b0f43
leandroti7 <leandro.dasilvati7@gmail.com>
2018-06-20 02:12:30 -0300

    config homestation and back developing leandro_page

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,14 @@ var destSaida =  'css';
 
 
 gulp.task('sass', function(){
-	return gulp.src(destOrigin)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(destSaida));
-	});
+    return gulp.src(destOrigin)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(destSaida));
+});
 
 gulp.task('watch',function(){
-	gulp.watch(destOrigin, ['sass']);
+    gulp.watch(destOrigin, ['sass']);
 
-	});
+});
 
 gulp.task('default',['sass','watch']);
-
